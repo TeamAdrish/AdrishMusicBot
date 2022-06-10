6696012b9fc5f20002b04ef43d2dd36774001176
Mr Adrish <105521183+TeamAdrish@users.noreply.github.com>
2022-06-11 04:18:18 +0530

    Create config.py

---
+++ modules/config.py
@@ -0,0 +1,33 @@
+# 𝐌𝐨𝐝𝐮𝐥𝐞𝐬 𝐚𝐧𝐝 𝐄𝐧𝐯𝐢𝐫𝐨𝐧𝐦𝐞𝐧𝐭𝐬
+import os
+import aiohttp
+from os import getenv
+from dotenv import load_dotenv
+
+# 𝐈𝐧𝐭𝐞𝐫𝐧𝐚𝐥 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 (@𝗔𝗱𝗿𝗶𝘀𝗵🇽𝗚𝗼𝗱)
+load_dotenv()
+que = {}
+admins = {}
+aiohttpsession = aiohttp.ClientSession()
+
+# 𝐑𝐞𝐪𝐮𝐢𝐫𝐞𝐝 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 //𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 @𝗔𝗱𝗿𝗶𝘀𝗵𝐍𝐞𝐭𝐰𝐨𝐫𝐤
+API_HASH = getenv("API_HASH", "XXXXX")
+API_ID = int(getenv("API_ID", "XXXXX"))
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "XXXXX")
+ASSISTANT_USERNAME = getenv("ASSISTANT_USERNAME", "XXXXX")
+BOT_IMAGE = getenv("BOT_IMAGE", "https://te.legra.ph/file/bcac4ce2a546c8012ebfa.jpg")
+BOT_NAME = getenv("BOT_NAME", "XXXXX")
+BOT_TOKEN = getenv("BOT_TOKEN", "12345:XXXXX")
+BOT_USERNAME = getenv("BOT_USERNAME", "XXXXX")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "120"))
+OWNER_NAME = getenv("OWNER_NAME", "𝗔𝗱𝗿𝗶𝘀𝗵🇽𝗚𝗼𝗱")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "AdrishXhog")
+SOURCE_CODE = getenv("SOURCE_CODE", "https://github.com/teamadrish/adrishmusicbot")
+STRING_SESSION = getenv("STRING_SESSION", "session")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5131191131").split()))
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/AdrishMusicSupport")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "https://t.me/AdrishMusicUpdates")
+
+# 𝐃𝐨 𝐍𝐨𝐭 𝐂𝐡𝐚𝐧𝐠𝐞 𝐓𝐡𝐢𝐬 𝐋𝐢𝐧𝐞𝐬 // 𝐈𝐠𝐧𝐨𝐫𝐞 𝐓𝐡𝐢𝐬 (𝐀𝐝𝐢𝐭𝐲𝐚 𝐇𝐚𝐥𝐝𝐞𝐫) 
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+PROFILE_CHANNEL = getenv("PROFILE_CHANNEL", "https://t.me/TheAdrish")
